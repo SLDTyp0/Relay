45c9cf5a8ece49528fc6529cb2da6f0a87964668
SLDTyp0 <164403125+SLDTyp0@users.noreply.github.com>
2024-04-24 15:45:38 +0000

    censoring orignal code

--- run.bat
+++ run.bat
@@ -3,11 +3,11 @@ title LAN World Relay
 echo:
 echo Eaglercraft 1.5.2 LAN World Relay
 echo Note: Requires java 8 or newer!
-:fuck
+:fudge
 timeout /t 10 /nobreak
 echo:
 java -jar EaglerSPRelay.jar --debug
 echo:
 echo RELAY STOPPED!
 echo Restarting...
-goto fuck
+goto fudge
